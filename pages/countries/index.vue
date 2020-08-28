fd0e243e7d9c4e9d218bcf043ec51793007f50d2
Amit Kesarwani <akumar00029@gmail.com>
2020-08-29 02:19:06 +0530

    work in progress added for later pages

--- pages/countries/index.vue
+++ pages/countries/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-     <h1 class="pa-4 display-2 bold my-3" align="center">Covid-19 Tracker Country Wice</h1>
+<!--     <h1 class="pa-4 display-2 bold my-3" align="center">Covid-19 Tracker Country Wice</h1>-->
 
-    <v-combobox :items="countries" dense solo color="primary" placeholder="Country Name" v-model="country" />
-    <Covid-Card :confirmed="123211" :deaths="313123" :recovered="2132121"/>
+<!--    <v-combobox :items="countries" dense solo color="primary" placeholder="Country Name" v-model="country" />-->
+<!--    <Covid-Card :confirmed="123211" :deaths="313123" :recovered="2132121"/>-->
+
+    <v-text-field color="success" loading disabled></v-text-field>
+
+    Work in progress
   </div>
 </template>
 
@@ -15,22 +19,14 @@
       data(){
         return{
           countries  :[
-            'India',
-            'America'
+
           ],
           country : "",
 
         }
       },
       async mounted(){
-        await this.$axios.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-27-2020.csv')
-          .then((res) => {
-             let records = res.data.split('\n')
-              records.forEach(v => {
-                  let data = v.split(',')
-                console.log(data)
-              })
-          })
+
       }
     }
 </script>
