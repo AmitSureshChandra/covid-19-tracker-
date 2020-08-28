c8c195c9a6b92a50341ccf981373fb761fadfd1c
Amit Kesarwani <akumar00029@gmail.com>
2020-08-28 07:23:15 +0530

    project created

---
+++ layouts/default.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app dark>
+    <v-toolbar
+      fixed
+      app
+      i
+      max-height="70px"
+    >
+      <v-app-bar-nav-icon v-show="false" />
+
+      <span class="title primary--text">
+        {{ title }}
+      </span>
+
+        <nuxt-link to="/home">Home</nuxt-link>
+        <nuxt-link to="/countries">Countries</nuxt-link>
+
+      </v-toolbar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}  covid-19 tracker</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+
+      title: 'Covid-19 Tracker'
+    }
+  }
+}
+</script>
+<style>
+  a{
+    text-decoration: none;
+    font-weight: bold;
+    margin-left: 10px;
+    margin-right: 20px;
+  }
+  .title {
+    margin-right: 30px;
+    font-size: 40px;
+  }
+</style>
---
+++ pages/home/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <h1 align="center">Covid-19 tracker</h1>
+
+      <v-row align="center">
+        <v-col align="center">
+          <v-card>
+            <v-card-title>
+              <v-col>Confirmed Cases</v-col>
+              <v-col>323</v-col>
+            </v-card-title>
+          </v-card>
+        </v-col>
+
+        <v-col align="center">
+         <v-card>
+           <v-card-title>
+             <v-col>Confirmed Cases</v-col>
+             <v-col>323</v-col>
+           </v-card-title>
+         </v-card>
+        </v-col>
+
+        <v-col align="center">
+          <v-card>
+            <v-card-title>
+              <v-col>Confirmed Cases</v-col>
+              <v-col>323</v-col>
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
