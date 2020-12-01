2877ed61df7f205c969f05b50b39dcb0f5db3036
Amit Sureshchandra Kesarwani <47358181+AmitSureshChandra@users.noreply.github.com>
2020-12-01 16:30:09 +0530

    meta updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,12 +16,12 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - Covid-19 Tracker ',
+    title: 'Covid-19 Tracker',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Country/State wise Covid-19 Tracker' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
